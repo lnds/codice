6fdec3b789268fb76fc4b15eda916bd72c6b457a
Eduardo Díaz <ediaz@dcc8090.cl>
2020-03-04 00:29:24 -0300

    add light blue lite css style and logo

--- codice/settings.py
+++ codice/settings.py
@@ -153,3 +153,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.environ.get('STATIC', '/static')
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
