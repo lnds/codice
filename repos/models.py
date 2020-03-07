5428147cfdfdae863b04f4380ffd8b97ee8bedea
Eduardo Díaz <ediaz@dcc8090.cl>
2020-03-07 12:24:28 -0300

    add repos

---
+++ repos/models.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext as _
+
+from codice import settings
+
+
+class Repository(models.Model):
+
+    git_url_validator = RegexValidator(r'((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?',
+                                       _("Invalid git url"))
+
+    class Status(models.IntegerChoices):
+        CREATED = 0
+        ERROR = 1
+        CLONING = 2
+        ANALYZING = 3
+        OK = 4
+
+    name = models.SlugField(max_length=40)
+    url = models.CharField(max_length=200, validators=[git_url_validator])
+    branches_to_track = models.CharField(max_length=200, blank=True)
+    default_branch = models.CharField(max_length=40, blank=True)
+    username = models.CharField(max_length=80, blank=True)
+    password = models.CharField(max_length=200, blank=True)
+    public = models.BooleanField(default=False)
+    base_directory = models.CharField(max_length=300, null=True)
+
+    status = models.IntegerField(choices=Status.choices)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'codice_repository'
+        unique_together = (('name', 'owner'), ('url', 'owner'),)
+
+    def __str__(self):
+        return self.name
+
+
+class Branch(models.Model):
+    name = models.CharField(max_length=200)
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'codice_branch'
+        unique_together = (('name', 'repository'),)
---
+++ repos/views.py
@@ -0,0 +1,64 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import Q
+from django.views.generic import ListView, CreateView, DetailView, DeleteView
+from django.utils.translation import gettext as _
+
+from repos.forms import RepositoryForm
+from repos.models import Repository
+
+'''Needed by of all Repository Views'''
+class RepositoryMixin(LoginRequiredMixin, SuccessMessageMixin):
+    model = Repository
+
+
+class CanSeeRepoMixin(PermissionRequiredMixin):
+    def has_permission(self):
+        self.owner = self.request.user
+        if 'pk' in self.kwargs:
+            self.repo = Repository.objects.get(pk=self.kwargs['pk'])
+            return self.repo.owner == self.owner or self.repo.public
+        return False
+
+
+class CanAdminReposMixin(CanSeeRepoMixin):
+    def has_permission(self):
+        return super().has_permission() and self.request.user.can_add_repo() and self.request.user.can_del_repo()
+
+'''Lists all repos'''
+class RepositoryList(RepositoryMixin, ListView):
+    context_object_name = 'repo_list'
+    template_name = 'repository/list.html'
+    paginate_by = 10
+    owner = None
+
+    def get_queryset(self):
+        self.owner = self.request.user
+        return Repository.objects.filter(Q(owner=self.owner) | Q(public=True))
+
+
+'''Add a new repo'''
+class RepositoryCreate(RepositoryMixin, CanAdminReposMixin, CreateView):
+    success_message = _('Repository was added successfully')
+    template_name = 'repository/add.html'
+    form_class = RepositoryForm
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.owner = self.request.user
+        return form
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        form.instance.status = Repository.Status.CREATED
+        self.object = form.save()
+        # clone_remote_repository.delay(self.object.owner.id, self.project.id, self.object.id)
+        return super(RepositoryCreate, self).form_valid(form)
+
+
+class RepositoryDetail(RepositoryMixin, CanSeeRepoMixin, DetailView):
+    template_name = 'repository/detail.html'
+
+
+class RepositoryDelete(RepositoryMixin, CanAdminReposMixin, DeleteView):
+    success_message = _('Repository was deleted successfully')
